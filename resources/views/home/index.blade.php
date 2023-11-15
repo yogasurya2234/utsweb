de367a3cd4dd8ce89a7c5cd4834c211c8343a9ea
yogasurya2234 <yogasurya069@gmail.com>
2023-11-15 19:45:44 +0800

    membuat home instiki

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.master')
+
+@section('title', 'Home')
+
+@section('breadcrumb')
+    <li class="breadcrumb-item active">Home</li>
+@endsection
+
+@section('content')
+    <h1>Selamat datang I Komang Yoga Surya Aditya</h1>
+@endsection
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,17 @@
+<header class="navbar navbar-expand-lg bg-body-tertiary py-3 mb-4 border-bottom">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Traveloke</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+    <ul class="nav nav-pills">
+        @php
+            $menu = [['url' => '/', 'name' => 'Home'], ['url' => 'Customer', 'name' => 'Customer'], ['url' => 'Barang', 'name' => 'Barang']];
+        @endphp
+
+        @foreach ($menu as $m)
+            @include('layout.nav-item', ['menu' => $m])
+        @endforeach
+    </ul>
+</header>
